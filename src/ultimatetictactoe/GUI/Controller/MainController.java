8d2be28b7d170bafeedf32053057f20273c6c725
Husso1997 <hussain1961@hotmail.com>
2018-02-26 10:17:28 +0100

    fixed

--- src/ultimatetictactoe/GUI/Controller/MainController.java
+++ src/ultimatetictactoe/GUI/Controller/MainController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ultimatetictactoe;
+package ultimatetictactoe.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author B
  */
-public class FXMLDocumentController implements Initializable {
+public class MainController implements Initializable {
     
     @FXML
     private Label label;
