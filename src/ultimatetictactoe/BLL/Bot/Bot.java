c9fd14db64e6aebcd7abd475cf1d5a52560e8423
davidmsf <davidmsf@gmail.com>
2018-03-05 11:26:30 +0100

    bot class

---
+++ src/ultimatetictactoe/BLL/Bot/Bot.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ultimatetictactoe.BLL.Bot;
+
+import ultimatetictactoe.BLL.Game.IGameState;
+import ultimatetictactoe.BLL.Move.IMove;
+
+/**
+ *
+ * @author B
+ */
+public class Bot implements IBot{
+
+    int x = 4;
+    int y = 4;
+    
+    @Override
+    public IMove doMove(IGameState state) {    
+        IMove iMove = new IMove() {
+            @Override
+            public int getX() {
+                return x;
+            }
+
+            @Override
+            public int getY() {
+                return y;
+            }
+        };
+            
+        return iMove;
+    }
+    
+    
+    
+}
