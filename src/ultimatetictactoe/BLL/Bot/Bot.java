f232f5f5322b3df3d65a1db65f0d0bbdced7196b
davidmsf <davidmsf@gmail.com>
2018-03-06 20:45:56 +0100

    bot minimax

--- src/ultimatetictactoe/BLL/Bot/Bot.java
+++ src/ultimatetictactoe/BLL/Bot/Bot.java
@@ -5,6 +5,7 @@
  */
 package ultimatetictactoe.BLL.Bot;
 
+import java.util.ArrayList;
 import java.util.List;
 import ultimatetictactoe.BLL.Game.IGameState;
 import ultimatetictactoe.BLL.Move.IMove;
@@ -18,14 +19,19 @@ public class Bot implements IBot{
  
     private IGameState currentState;
     private String[][] board;
+    private IMove firstMove;
+    private int x;
+    private int y;
     
     @Override
     public IMove doMove(IGameState state) {    
         currentState = state;
         board = currentState.getField().getBoard();
+        getFirstMove();
+        int bestValue = negaMax(firstMove, 4, 1, 1);
+        System.out.println("!!!POINT: "+bestValue+" "+x+" "+y);
         
-        int x = 1;
-        int y = 1;
+
         IMove iMove = new IMove() {
             @Override
             public int getX() {
@@ -43,7 +49,7 @@ public class Bot implements IBot{
     
     public int negaMax(IMove move, int depth, int player, int rootPlayer)
     {
-
+        
         if(checkWinner(move, xOrO(player)))
         {
             int points = player*calcPoints(player, rootPlayer);
@@ -61,8 +67,14 @@ public class Bot implements IBot{
         {
             
             board[cell.getX()][cell.getY()] = xOrO(player);
-            int currentValue = -negaMax(move, depth-1, -player, rootPlayer);
+            printBoard();
+            int currentValue = -negaMax(cell, depth-1, -player, rootPlayer);
             bestValue = Math.max(bestValue, currentValue);
+            if((currentValue == bestValue) && depth == 4)
+            {
+                x = cell.getX();
+                y = cell.getY();
+            }
             board[cell.getX()][cell.getY()] = ".";
         }
         return bestValue;
@@ -104,16 +116,20 @@ public class Bot implements IBot{
         {
             integerY = localY*3;
         }
-
+           
             for(int i = integerI;i<3+integerI;i++)
             {
                 for(int y = integerY;y<3+integerY;y++)
                 {
-                    if(board[i][y].equals(board[i][y]) 
-                    && board[i][y].equals(board[i][y]) 
-                    && board[i][y].equals(player))
+                    if(y % 3 == 0 || y == 0)
                     {
-                        return true;
+                        
+                        if(board[i][y].equals(board[i][y+1]) 
+                        && board[i][y+1].equals(board[i][y+2]) 
+                        && board[i][y].equals(player))
+                        {
+                            return true;
+                        }
                     }
                 }
             }
@@ -122,11 +138,14 @@ public class Bot implements IBot{
             {
                 for(int i = integerY;i<3+integerY;i++)
                 {
-                    if(board[i][y].equals(board[i][y]) 
-                    && board[i][y].equals(board[i][y]) 
-                    && board[i][y].equals(player))
+                    if(i % 3 == 0 || i == 0)
                     {
-                        return true; 
+                        if(board[i][y].equals(board[i+1][y]) 
+                        && board[i+1][y].equals(board[i+2][y]) 
+                        && board[i][y].equals(player))
+                        {
+                            return true; 
+                        }
                     }
                 }
             }
@@ -135,15 +154,241 @@ public class Bot implements IBot{
     }
 
     private List<IMove> getAvailableCells(IMove move) {
+        int localX = move.getX() % 3;
+        int localY = move.getY() % 3;
         
+        int integerI = 0;
+        int integerY = 0;
+        
+        List<IMove> availableCells = new ArrayList();
+        if(localX != 0)
+        {
+            integerI = localX*3;
+        }
+        if(localY != 0)
+        {
+            integerY = localY*3;
+        }
+
+            for(int i = integerI;i<3+integerI;i++)
+            {
+                for(int q = integerY;q<3+integerY;q++)
+                {
+                    int x = i;
+                    int y = q;
+                    
+                    if(board[i][y].equals("."))
+                    {
+                        availableCells.add(new IMove() {
+                            @Override
+                            public int getX() {
+                                return x;
+                            }
+
+                            @Override
+                            public int getY() {
+                                return y;
+                            }
+                        });
+                    }
+                }
+            }
+            
+        return availableCells;
     }
 
-    private int calcPoints(int player, int rootPlayer) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private int calcPoints(int player, int rootPlayer) 
+    {
+        int point = 0;
+        if(player == rootPlayer){
+            point = 10;
+        }
+        else
+        {
+            point = 1;
+        }
+        return point;
+
     }
 
     private int checkPossiblekWinner(int rootPlayer) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int point = 2;
+        
+        //Diagonal in multidimentional array
+        point += diagonalCheckLeftToRight(xOrO(rootPlayer)); 
+        point += diagonalCheckRightToLeft(xOrO(rootPlayer));
+        //sideways og downward in multidimentional array
+        point += verticalCheck(xOrO(rootPlayer));
+        point += horizontalCheck(xOrO(rootPlayer));
+        
+        return point;
+    }
+    
+    
+    private int diagonalCheckLeftToRight(String player) 
+    {
+        int point = 0;
+        
+        for(int x = 0; x < 7; x++)
+        {
+            if(x % 3 == 0 || x == 0)
+            {
+                for(int y = 0; y < 7; y++)
+                {
+                    if(y % 3 == 0 || y == 0)
+                    {
+                        if(board[x][y].equals(board[x+1][y+1]) && board[x][y].equals(player)) 
+                        {
+                            point += 4;
+                        }
+                        if(board[x+1][y+1].equals(board[x+2][y+2]) && board[x+1][y+1].equals(player)) 
+                        {
+                            point += 4;
+                        }
+                    }
+
+                }
+            }
+        }
+        
+       return point;
+
+        
+    }
+  
+    
+    
+    private int diagonalCheckRightToLeft(String player) 
+    {
+        int point = 0;
+        
+        for(int x = 0; x < 7; x++)
+        {
+            if(x % 3 == 0 || x == 0)
+            {
+                for(int y = 0; y < 7; y++)
+                {
+                    if(y % 3 == 0 || y == 0)
+                    {
+                        
+                        if(board[x][y+2].equals(board[x+1][y+1]) && board[x][y+2].equals(player)) 
+                        {
+                            point += 4;
+                        }
+                        if(board[x+1][y+1].equals(board[x+2][y]) && board[x+1][y+1].equals(player)) 
+                        {
+                            point += 4;
+                        }
+                        
+                    }
+
+                }
+            }
+        }
+        
+        return point;
+
+    }
+    
+    
+    
+    private int verticalCheck(String player)
+    {
+        int point = 0;
+        for(int x = 0; x < 9; x++)
+        {
+            for(int y = 0; y < 7; y++)
+            {
+                if(y % 3 == 0 || y == 0)
+                {
+                    
+                    if(board[x][y].equals(board[x][y+1]) && board[x][y].equals(player)) 
+                    {
+                        point += 4;
+                    }
+                    if(board[x][y+1].equals(board[x][y+2]) && board[x][y+1].equals(player)) 
+                    {
+                        point += 4;
+
+                    }
+                    
+                }
+
+            }
+        }
+        return point;
+    }
+    
+    
+    
+    private int horizontalCheck(String player)
+    {
+        int point = 0;
+        for(int y = 0; y < 9; y++)
+        {
+            for(int x = 0; x < 7; x++)
+            {
+                if(x % 3 == 0 || x == 0)
+                {
+                    
+                    if(board[x][y].equals(board[x+1][y]) && board[x][y].equals(player)) 
+                    {
+                        point += 4;
+
+                    }
+                    if(board[x+1][y].equals(board[x+2][y]) && board[x+1][y].equals(player)) 
+                    {
+                        point += 4;
+                    }
+                }
+
+            }
+        }
+        return point;
+    }
+    
+    
+    
+    private void printBoard()
+    {
+        for(int x = 0; x < 9; x++)
+        {
+            for(int y = 0; y < 9; y++)
+            {
+                System.out.print(board[x][y]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("");
+    }
+
+    private void getFirstMove() {
+        String[][] macroBoard = currentState.getField().getMacroboard();
+        for(int x = 0; x < macroBoard.length; x++)
+        {
+            for(int y = 0; y < macroBoard.length; y++)
+            {
+                if(macroBoard[x][y].equals("-1"))
+                {
+                    int i = x;
+                    int q = y;
+                    firstMove = new IMove() {
+                        @Override
+                        public int getX() {
+                            return i;
+                        }
+
+                        @Override
+                        public int getY() {
+                            return q;
+                        }
+                    };
+                    
+                }
+            }
+        }
+        
     }
     
 }
