28bb9b550a5b423e4c48102a185d69c36aebc436
davidmsf <davidmsf@gmail.com>
2018-03-07 22:00:23 +0100

    blocking !!! wharh

--- src/ultimatetictactoe/BLL/Bot/Bot.java
+++ src/ultimatetictactoe/BLL/Bot/Bot.java
@@ -20,7 +20,7 @@ public class Bot implements IBot{
     private IGameState currentState;
     private String[][] board;
     private IMove firstMove;
-    private final int DEPTH = 6;
+    private final int DEPTH = 2;
     private final int THREEONROW = 30;
     private int x;
     private int y;
@@ -73,11 +73,16 @@ public class Bot implements IBot{
             board[cell.getX()][cell.getY()] = xOrO(player);
             int currentValue = -negaMax(cell, currentDepth-1, -player, rootPlayer);
             bestValue = Math.max(bestValue, currentValue);
+            if(currentDepth == DEPTH)
+            {
+                System.out.println("------------ X: "+cell.getX()+" Y"+cell.getY()+" SCORE:"+currentValue+" ------------------");
+            }
             if((currentValue == bestValue) && currentDepth == DEPTH)
             {
                 x = cell.getX();
                 y = cell.getY();
             }
+            
             board[cell.getX()][cell.getY()] = ".";
         }
         return bestValue;
@@ -195,10 +200,7 @@ public class Bot implements IBot{
         point -= verticalCheck("O");
         point += horizontalCheck("X");
         point -= horizontalCheck("O");
-        if(point < 0){
-            printBoard();
-            System.out.println(point);
-        }
+
         return point;
     }
     
@@ -215,25 +217,45 @@ public class Bot implements IBot{
                 {
                     if(y % 3 == 0 || y == 0)
                     {
-                        /*
+                        
                         if(board[x][y].equals(board[x+1][y+1]) && board[x][y].equals(player)) 
                         {
-                            point += 4;
+                            point += 2;
                         }
                         if(board[x+1][y+1].equals(board[x+2][y+2]) && board[x+1][y+1].equals(player)) 
                         {
-                            point += 4;
+                            point += 2;
                         }
-                        */
+                        
                         if(board[x][y].equals(board[x+1][y+1]) 
                         && board[x+1][y+1].equals(board[x+2][y+2]) 
                         && board[x][y].equals(player))
                         {
                             point += THREEONROW;
-                            if(player.equals("O"))
+                          
+                        }
+                        
+                        if(player.equals("X")){
+                            if(board[x][y].equals(board[x+1][y+1]) 
+                            && board[x][y].equals("O") 
+                            && board[x+2][y+2].equals("X"))
                             {
-                                point += 10;
-     
+
+                                point += THREEONROW;
+                            }
+                            if(board[x][y].equals(board[x+2][y+2]) 
+                            && board[x][y].equals("O") 
+                            && board[x+1][y+1].equals("X"))
+                            {
+
+                                point += THREEONROW;
+                            }
+                            if(board[x+1][y+1].equals(board[x+2][y+2]) 
+                            && board[x+1][y+1].equals("O") 
+                            && board[x][y].equals("X"))
+                            {
+
+                                point += THREEONROW;
                             }
                         }
                          
@@ -262,24 +284,44 @@ public class Bot implements IBot{
                 {
                     if(y % 3 == 0 || y == 0)
                     {
-                        /*
+                        
                         if(board[x][y+2].equals(board[x+1][y+1]) && board[x][y+2].equals(player)) 
                         {
-                            point += 4;
+                            point += 2;
                         }
                         if(board[x+1][y+1].equals(board[x+2][y]) && board[x+1][y+1].equals(player)) 
                         {
-                            point += 4;
+                            point += 2;
                         }
-                        */
+                        
                         if(board[x][y+2].equals(board[x+1][y+1]) 
                         && board[x+1][y+1].equals(board[x+2][y]) 
                         && board[x][y+2].equals(player))
                         {
                             point += THREEONROW;
-                            if(player.equals("O"))
+                        }
+                        
+                        if(player.equals("X")){
+                            if(board[x][y+2].equals(board[x+1][y+1]) 
+                            && board[x][y+2].equals("O") 
+                            && board[x+2][y].equals("X"))
                             {
-                                point += 10;
+
+                                point += THREEONROW;
+                            }
+                            if(board[x][y+2].equals(board[x+2][y]) 
+                            && board[x][y+2].equals("O") 
+                            && board[x+1][y+1].equals("X"))
+                            {
+
+                                point += THREEONROW;
+                            }
+                            if(board[x+1][y+1].equals(board[x+2][y]) 
+                            && board[x+1][y+1].equals("O") 
+                            && board[x][y+2].equals("X"))
+                            {
+
+                                point += THREEONROW;
                             }
                         }
                     }
@@ -303,25 +345,47 @@ public class Bot implements IBot{
             {
                 if(y % 3 == 0 || y == 0)
                 {
-                    /*
+                    
                     if(board[x][y].equals(board[x][y+1]) && board[x][y].equals(player)) 
                     {
-                        point += 4;
+                        point += 2;
                     }
                     if(board[x][y+1].equals(board[x][y+2]) && board[x][y+1].equals(player)) 
                     {
-                        point += 4;
+                        point += 2;
 
                     }
-                    */
+                    
                     if(board[x][y].equals(board[x][y+1]) 
                     && board[x][y+1].equals(board[x][y+2]) 
                     && board[x][y].equals(player))
                     {
                         point += THREEONROW;
-                        if(player.equals("O"))
+                    }
+                    
+                    
+                    
+                    if(player.equals("X")){
+                        if(board[x][y].equals(board[x][y+1]) 
+                        && board[x][y].equals("O") 
+                        && board[x][y+2].equals("X"))
                         {
-                            point += 10;
+
+                            point += THREEONROW;
+                        }
+                        if(board[x][y].equals(board[x][y+2]) 
+                        && board[x][y].equals("O") 
+                        && board[x][y+1].equals("X"))
+                        {
+
+                            point += THREEONROW;
+                        }
+                        if(board[x][y+1].equals(board[x][y+2]) 
+                        && board[x][y+1].equals("O") 
+                        && board[x][y].equals("X"))
+                        {
+
+                            point += THREEONROW;
                         }
                     }
                     
@@ -343,30 +407,49 @@ public class Bot implements IBot{
             {
                 if(x % 3 == 0 || x == 0)
                 {
-                    /*
+                    
                     if(board[x][y].equals(board[x+1][y]) && board[x][y].equals(player)) 
                     {
-                        point += 4;
+                        point += 2;
 
                     }
                     if(board[x+1][y].equals(board[x+2][y]) && board[x+1][y].equals(player)) 
                     {
-                        point += 4;
+                        point += 2;
                     }
-                    */
+                    
 
 
+                    if(board[x][y].equals(board[x+1][y]) 
+                    && board[x+1][y].equals(board[x+2][y]) 
+                    && board[x][y].equals(player))
+                    {
+                        point += THREEONROW;
+                    }
+                    
+                    if(player.equals("X")){
                         if(board[x][y].equals(board[x+1][y]) 
-                        && board[x+1][y].equals(board[x+2][y]) 
-                        && board[x][y].equals(player))
+                        && board[x][y].equals("O") 
+                        && board[x+2][y].equals("X"))
                         {
 
                             point += THREEONROW;
-                            if(player.equals("O"))
-                            {
-                                point += 10;
-                            }
                         }
+                        if(board[x][y].equals(board[x+2][y]) 
+                        && board[x][y].equals("O") 
+                        && board[x+1][y].equals("X"))
+                        {
+
+                            point += THREEONROW;
+                        }
+                        if(board[x+1][y].equals(board[x+2][y]) 
+                        && board[x+1][y].equals("O") 
+                        && board[x][y].equals("X"))
+                        {
+
+                            point += THREEONROW;
+                        }
+                    }
                     
                 }
 
