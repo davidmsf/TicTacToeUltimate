cc26ff6bb1c8fcaf49e33bde46285f8976946661
davidmsf <davidmsf@gmail.com>
2018-03-07 19:32:03 +0100

    new

--- src/ultimatetictactoe/BLL/Bot/Bot.java
+++ src/ultimatetictactoe/BLL/Bot/Bot.java
@@ -20,6 +20,8 @@ public class Bot implements IBot{
     private IGameState currentState;
     private String[][] board;
     private IMove firstMove;
+    private final int DEPTH = 6;
+    private final int THREEONROW = 30;
     private int x;
     private int y;
     
@@ -29,7 +31,7 @@ public class Bot implements IBot{
         board = currentState.getField().getBoard();
         
         getFirstMove();
-        int bestValue = negaMax(firstMove, 4, 1, 1);
+        int bestValue = negaMax(firstMove, DEPTH, 1, 1);
         System.out.println("!!!POINT: "+bestValue+" "+x+" "+y);
         
 
@@ -48,15 +50,16 @@ public class Bot implements IBot{
         return iMove;
     }
     
-    public int negaMax(IMove move, int depth, int player, int rootPlayer)
+    public int negaMax(IMove move, int currentDepth, int player, int rootPlayer)
     {
+        
         
         if(checkWinner(xOrO(player)))
         {
             int points = player*calcPoints(player, rootPlayer);
             return points;
         }
-        else if(depth == 0 || getAvailableCells(move).isEmpty())
+        else if(currentDepth == 0 || getAvailableCells(move).isEmpty())
         {
             int points = player*checkPossiblekWinner();
             return points;
@@ -68,9 +71,9 @@ public class Bot implements IBot{
         {
             
             board[cell.getX()][cell.getY()] = xOrO(player);
-            int currentValue = -negaMax(cell, depth-1, -player, rootPlayer);
+            int currentValue = -negaMax(cell, currentDepth-1, -player, rootPlayer);
             bestValue = Math.max(bestValue, currentValue);
-            if((currentValue == bestValue) && depth == 4)
+            if((currentValue == bestValue) && currentDepth == DEPTH)
             {
                 x = cell.getX();
                 y = cell.getY();
@@ -187,12 +190,15 @@ public class Bot implements IBot{
         point -= diagonalCheckLeftToRight("O"); 
         point += diagonalCheckRightToLeft("X");
         point -= diagonalCheckRightToLeft("O");
-        //sideways og downward in multidimentional array
+        //sideways and downward in multidimentional array
         point += verticalCheck("X");
         point -= verticalCheck("O");
         point += horizontalCheck("X");
         point -= horizontalCheck("O");
-        
+        if(point < 0){
+            printBoard();
+            System.out.println(point);
+        }
         return point;
     }
     
@@ -209,6 +215,7 @@ public class Bot implements IBot{
                 {
                     if(y % 3 == 0 || y == 0)
                     {
+                        /*
                         if(board[x][y].equals(board[x+1][y+1]) && board[x][y].equals(player)) 
                         {
                             point += 4;
@@ -217,12 +224,17 @@ public class Bot implements IBot{
                         {
                             point += 4;
                         }
-                    
+                        */
                         if(board[x][y].equals(board[x+1][y+1]) 
                         && board[x+1][y+1].equals(board[x+2][y+2]) 
                         && board[x][y].equals(player))
                         {
-                            point += 20;
+                            point += THREEONROW;
+                            if(player.equals("O"))
+                            {
+                                point += 10;
+     
+                            }
                         }
                          
                     }
@@ -250,7 +262,7 @@ public class Bot implements IBot{
                 {
                     if(y % 3 == 0 || y == 0)
                     {
-                        
+                        /*
                         if(board[x][y+2].equals(board[x+1][y+1]) && board[x][y+2].equals(player)) 
                         {
                             point += 4;
@@ -259,12 +271,16 @@ public class Bot implements IBot{
                         {
                             point += 4;
                         }
-                        
+                        */
                         if(board[x][y+2].equals(board[x+1][y+1]) 
                         && board[x+1][y+1].equals(board[x+2][y]) 
                         && board[x][y+2].equals(player))
                         {
-                            point += 10;
+                            point += THREEONROW;
+                            if(player.equals("O"))
+                            {
+                                point += 10;
+                            }
                         }
                     }
 
@@ -287,7 +303,7 @@ public class Bot implements IBot{
             {
                 if(y % 3 == 0 || y == 0)
                 {
-                    
+                    /*
                     if(board[x][y].equals(board[x][y+1]) && board[x][y].equals(player)) 
                     {
                         point += 4;
@@ -297,12 +313,16 @@ public class Bot implements IBot{
                         point += 4;
 
                     }
-                    
+                    */
                     if(board[x][y].equals(board[x][y+1]) 
                     && board[x][y+1].equals(board[x][y+2]) 
                     && board[x][y].equals(player))
                     {
-                        point += 10;
+                        point += THREEONROW;
+                        if(player.equals("O"))
+                        {
+                            point += 10;
+                        }
                     }
                     
                 }
@@ -323,7 +343,7 @@ public class Bot implements IBot{
             {
                 if(x % 3 == 0 || x == 0)
                 {
-                    
+                    /*
                     if(board[x][y].equals(board[x+1][y]) && board[x][y].equals(player)) 
                     {
                         point += 4;
@@ -333,18 +353,21 @@ public class Bot implements IBot{
                     {
                         point += 4;
                     }
-                    
-                    if(x % 3 == 0 || x == 0)
-                    {
+                    */
+
 
                         if(board[x][y].equals(board[x+1][y]) 
                         && board[x+1][y].equals(board[x+2][y]) 
                         && board[x][y].equals(player))
                         {
 
-                            point += 10;
+                            point += THREEONROW;
+                            if(player.equals("O"))
+                            {
+                                point += 10;
+                            }
                         }
-                    }
+                    
                 }
 
             }
