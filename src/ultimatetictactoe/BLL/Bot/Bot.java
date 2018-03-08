26fd6e604e269a480f69c26f14ba3efc5d306f98
Husso1997 <hussain1961@hotmail.com>
2018-03-08 09:38:58 +0100

    .

--- src/ultimatetictactoe/BLL/Bot/Bot.java
+++ src/ultimatetictactoe/BLL/Bot/Bot.java
@@ -16,10 +16,17 @@ public class Bot implements IBot{
 
     int x = 4;
     int y = 4;
-    
+    String macroBoard [][];
+    String board[][];
+    IGameState gameState;
     @Override
-    public IMove doMove(IGameState state) {    
-        IMove iMove = new IMove() {
+    public IMove doMove(IGameState state) 
+    {    
+        macroBoard = state.getField().getMacroboard();
+        board = state.getField().getBoard();
+        gameState = state;
+        IMove iMove = new IMove() 
+        {
             @Override
             public int getX() {
                 return x;
@@ -32,6 +39,25 @@ public class Bot implements IBot{
         };
             
         return iMove;
+    }
+    
+    public void getAvailableField()
+    {
+        for(int i = 0;i<3;i++)
+        {
+            for(int y = 0;y<3;y++)
+            {
+                if(macroBoard[i][y].equals("-1"))
+                {
+                    int ii = i*3;
+                    if(board[ii].equals("."))
+                    {
+                        
+                    }
+                    
+                }
+            }
+        }
     }
     
     
